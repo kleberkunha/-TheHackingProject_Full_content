aece8504d9edb47d7834aeed0a68c2e8652d988f
kleber cunha <kleberkunha@hotmail.com>
2021-04-13 16:26:36 +0200

    Exercises 11 + folder 12

--- TheHackingProject_FUll_content/Ruby_Exercises_Tuesday/exo_11.rb
+++ TheHackingProject_FUll_content/Ruby_Exercises_Tuesday/exo_11.rb
@@ -1,9 +1,14 @@
 puts "Type the year you were born:"
-year = gets.chomp()
-age = 0
-
-for i in year.to_i..2021
-    puts i
-    year = year + year
-
+year_birth = gets.chomp()
+age = 2021 - year_birth.to_i
+puts "Year chosen is :" + year_birth
+years = 0
+puts "Your have #{age}"
+for i in years..age.to_i
+    puts"at #{years} years ago you had #{age} years"
+    age -= 1
+    years += 1
 end
+
+
+
